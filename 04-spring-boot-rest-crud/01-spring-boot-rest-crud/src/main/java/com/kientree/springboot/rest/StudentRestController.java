feab7a1cbfec73f557cac0a6c059e05f10cf5fad
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-04-30 03:09:40 -0700

    added spring boot rest path variables

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/kientree/springboot/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/kientree/springboot/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.kientree.springboot.rest;
 
 import com.kientree.springboot.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +14,32 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // Define endpoint for "/students" - return a list of students
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-
-        List<Student> students = new ArrayList<>();
+    // Define @PostConstruct to load the student data...only once!
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
 
         students.add(new Student("John", "Doe"));
         students.add(new Student("Alice", "Wonderful"));
         students.add(new Student("Lucy", "Wang"));
+    }
+
+    // Define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return students;
     }
+
+    // Define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list...keep ut simple for now
+
+        return students.get(studentId);
+    }
+
 }
