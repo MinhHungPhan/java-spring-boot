f87d7084f429cb4fd40dd063b3eacdb519f4a280
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-04 02:55:23 -0700

    added GET mapping for employee by ID in EmployeeRestController

--- 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee/src/main/java/com/kientree/springboot/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee/src/main/java/com/kientree/springboot/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.kientree.springboot.entity.Employee;
 import com.kientree.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,19 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw  new RuntimeException("Employeed id is not found - " + employeeId);
+        }
+
+        return  employee;
     }
 
 }
