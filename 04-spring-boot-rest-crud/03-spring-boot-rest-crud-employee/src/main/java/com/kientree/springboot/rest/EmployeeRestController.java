313bcd5f0c9561de235c335b413c9caaa86cb213
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-05 05:14:47 -0700

    added DELETE mapping for deleting an employee by ID

--- 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee/src/main/java/com/kientree/springboot/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee/src/main/java/com/kientree/springboot/rest/EmployeeRestController.java
@@ -63,4 +63,21 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    // Add mapping for DELETE /employees/{employeeId} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee employee = employeeService.findById(employeeId);
+
+        // Throw exception if null
+
+        if(employee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 }
