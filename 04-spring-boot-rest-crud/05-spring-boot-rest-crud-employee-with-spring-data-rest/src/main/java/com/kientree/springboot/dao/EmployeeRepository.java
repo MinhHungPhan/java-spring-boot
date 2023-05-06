bdaf71f192086689d0377c75ddee522cbd482771
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-06 01:53:37 -0700

    added @RepositoryRestResource annotation to EmployeeRepository

--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/kientree/springboot/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/kientree/springboot/dao/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.kientree.springboot.dao;
 import com.kientree.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path="members")
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
