0c52fb5a182d7f8b022f96f83673bd55239ed07f
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-08 13:55:50 -0700

    added EmployeeDAO interface

---
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-employee/src/main/java/com/kientree/springboot/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.kientree.springboot.dao;
+
+import com.kientree.springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+
+}
