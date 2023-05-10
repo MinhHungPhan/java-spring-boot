63f19ea4dc60bf029fcabe7e31f55ccd92eaae1c
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-10 01:55:11 -0700

    updated SecurityConfig class to restrict URLs based on Roles

--- 05-spring-boot-rest-security/01-spring-boot-rest-security-employee/src/main/java/com/kientree/springboot/security/SecurityConfig.java
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-employee/src/main/java/com/kientree/springboot/security/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.kientree.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +34,27 @@ public class SecurityConfig {
                 .build();
 
         return  new InMemoryUserDetailsManager(john,lucy,susan);
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws  Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // Use HTTP Basic Authentication
+        http.httpBasic();
+
+        // Disable Cross Site Request Forgery (CSRF)
+        // In general, CSRF protection is not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf().disable();
+
+        return http.build();
     }
 }
