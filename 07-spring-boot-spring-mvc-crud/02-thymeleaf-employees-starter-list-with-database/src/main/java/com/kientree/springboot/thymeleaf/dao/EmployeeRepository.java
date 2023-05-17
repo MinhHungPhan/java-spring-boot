9d5e69cb9f5d7d39c38ae371c67d7beb84a98960
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-17 01:25:59 -0700

    added method in EmployeeRepository to sort employees by last name

--- 07-spring-boot-spring-mvc-crud/02-thymeleaf-employees-starter-list-with-database/src/main/java/com/kientree/springboot/thymeleaf/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/02-thymeleaf-employees-starter-list-with-database/src/main/java/com/kientree/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -3,5 +3,11 @@ package com.kientree.springboot.thymeleaf.dao;
 import com.kientree.springboot.thymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // Add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
+
 }
