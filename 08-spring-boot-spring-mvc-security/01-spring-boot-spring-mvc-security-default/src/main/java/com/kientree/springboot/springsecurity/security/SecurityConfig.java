e1d9b441f4a19a4da519874a5da3714f9e752bee
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-21 08:25:04 -0700

    added SecurityConfig class for basic authentication

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/kientree/springboot/springsecurity/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.kientree.springboot.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        UserDetails lucy = User.builder()
+                .username("lucy")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        return  new InMemoryUserDetailsManager(john,lucy,susan);
+    }
+}
