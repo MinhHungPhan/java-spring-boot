61a84d7bb7c7671176886871dcfd9e800472cbf6
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-28 02:23:02 -0700

    updated SecurityConfig class to use JDBC

--- 08-spring-boot-spring-mvc-security/02-spring-boot-spring-mvc-security-jdbc-authentication/src/main/java/com/kientree/springboot/springsecurity/security/SecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/02-spring-boot-spring-mvc-security-jdbc-authentication/src/main/java/com/kientree/springboot/springsecurity/security/SecurityConfig.java
@@ -3,36 +3,20 @@ package com.kientree.springboot.springsecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    // Add support for JDBC ...no more hardcoded users
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        UserDetails lucy = User.builder()
-                .username("lucy")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        return  new InMemoryUserDetailsManager(john,lucy,susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
