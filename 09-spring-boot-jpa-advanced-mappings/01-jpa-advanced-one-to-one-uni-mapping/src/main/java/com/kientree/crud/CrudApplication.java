22627b5a1f8c2e47b6b14854ad5399b9482791e7
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-31 01:03:58 -0700

    added CommandLineRunner bean

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-advanced-one-to-one-uni-mapping/src/main/java/com/kientree/crud/CrudApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-advanced-one-to-one-uni-mapping/src/main/java/com/kientree/crud/CrudApplication.java
@@ -1,7 +1,9 @@
 package com.kientree.crud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -10,4 +12,11 @@ public class CrudApplication {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
