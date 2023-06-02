224399bf575f3187e1795d4460ec05c28b1ab2f6
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-06-02 02:38:39 -0700

    added method to create and save an instructor with associated details

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-advanced-one-to-one-uni-mapping/src/main/java/com/kientree/crud/CrudApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-advanced-one-to-one-uni-mapping/src/main/java/com/kientree/crud/CrudApplication.java
@@ -1,5 +1,8 @@
 package com.kientree.crud;
 
+import com.kientree.crud.dao.AppDAO;
+import com.kientree.crud.entity.Instructor;
+import com.kientree.crud.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,32 @@ public class CrudApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		// Create the instructor
+		Instructor instructor = new Instructor("John", "Doe", "john.doe@kientree.com");
+
+		// Create the instructor detail
+		InstructorDetail instructorDetail = new InstructorDetail("https://www.youtube.com/johndoe", "Guitar");
+
+		// Associate the instructor detail with the instructor
+		instructor.setInstructorDetail(instructorDetail);
+
+		// Save the instructor using the AppDAO
+
+		// Note: this will also save the InstructorDetail object because of CascadeType.all
+
+		System.out.println("Saving instructor: " + instructor);
+
+		appDAO.save(instructor);
+
+		System.out.println("Done!");
 	}
 }
