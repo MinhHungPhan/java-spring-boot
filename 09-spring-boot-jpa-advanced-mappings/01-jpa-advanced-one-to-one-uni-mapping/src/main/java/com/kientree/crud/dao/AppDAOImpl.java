f3ee5cd96110520bdeeb62c6aed6b4e5fe4883d6
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-06-02 02:36:24 -0700

    added AppDAOImpl class

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-advanced-one-to-one-uni-mapping/src/main/java/com/kientree/crud/dao/AppDAOImpl.java
@@ -0,0 +1,23 @@
+package com.kientree.crud.dao;
+
+import com.kientree.crud.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // Define field for entity manager
+    private EntityManager entityManager;
+
+    // Inject entity manager using constructor injection
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
