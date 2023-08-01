5f61a848efdf2367503ca8b04c42b0313e3cda77
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-08-01 17:45:02 +0200

    enhanced code readability by adding spaces

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/kientree/springcore/common/FootballCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/kientree/springcore/common/FootballCoach.java
@@ -8,6 +8,7 @@ public class FootballCoach implements Coach{
     public FootballCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+    
     @Override
     public String getDailyWorkout() {
         return "Incorporate ladder drills to improve your footwork and agility on the field";
